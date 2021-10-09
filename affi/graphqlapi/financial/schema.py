3ccc50c78a5d1ed59015fd83a954a0f434c81996
Mohammad <darbanhandrew@gmail.com>
2021-10-09 16:22:27 +0330

    resolve tranaction types

--- affi/graphqlapi/financial/schema.py
+++ affi/graphqlapi/financial/schema.py
@@ -5,11 +5,12 @@ from graphene_django.filter import DjangoFilterConnectionField
 from .types import WalletNode, PlainTextNode, TransactionNode
 from ...financial.models import Wallet
 
+
 class FinancialQuery(graphene.ObjectType):
     wallet = graphene.Field(WalletNode, user_id=graphene.Int())
     # all_wallet = DjangoFilterConnectionField(WalletNode)
 
-    transaction = PlainTextNode.Field(TransactionNode)
+    # transaction = PlainTextNode.Field(TransactionNode)
 
     def resolve_wallet(self, info, user_id):
         return Wallet.objects.filter(user__pk=user_id).first()
